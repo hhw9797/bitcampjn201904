bdc58b65bd2d668d38cd55234f244bbad97ea1f8
hhw9797 <hhw9797@naver.com>
2019-07-04 11:57:38 +0900

    20190704 sql
    그룹함수

---
+++ sql/20190704.sql
@@ -0,0 +1,89 @@
+-- 2019.07.04
+
+-- 그룹함수
+
+-- SUM(컬럼) : 그룹의 합을 반환
+
+-- 사원 테이블에서 사원의 월 급여(sal)의 총합을 구해보자
+select sum(sal) as "월 총급여"
+from emp
+;
+
+-- 10번 부서 소속 직원들의 월 급여 총액을 구해보자
+select sum(sal)
+from emp
+where deptno = 10
+;
+
+-- sum 함수에서 null 값 처리방식 : null 값은 무시한다.
+select sum(comm)
+from emp
+;
+select comm from emp;
+
+-- avg(컬럼) : 그룹에 포함된 값들의 평균값을 반환
+select avg(sal) as "전체사원의 평균 월급여"
+from emp
+;
+-- 그룹함수의 결과는 단일행의 하나의 컬럼
+select trunc(avg(sal)) as "전체사원의 평균 월급여"
+from emp
+;
+select trunc(avg(sal)) as "매니저의 평균 월급여"
+from emp
+where job='MANAGER'
+;
+
+-- avg 함수도 컬럼의 값이 null 일때 제외하고 처리
+select avg(comm)
+from emp
+;
+
+-- MAX(컬럼) : 최댓값 반환, MIN(컬럼) : 최솟값 반환
+select max(sal) as "최대급여", min(sal) as "최소급여"
+from emp
+;
+
+-- count(컬럼) : row의 갯수를 반환
+
+-- 회사에 소속된 사원의 수
+select count(*) as "전체 사원의 수"
+from emp
+;
+
+-- 10번 부서의 소속된 직원의 수
+select count(*)
+from emp
+where deptno = 10
+;
+
+-- 커미션을 받는 직원의 수
+select count(comm)
+from emp
+;
+
+-- 부서의 수를 구하자.
+select count(*)
+from dept
+;
+-- 사원들이 소속된 부서의 수
+select count(distinct deptno)
+from emp
+;
+
+select count(distinct job) as "업무 수"
+from emp
+;
+
+-- grouop by 컬럼이름 : row를 그룹핑 해서 처리, 집합함수(sum, avg, count..)
+-- 컬럼이름 -> 그룹핑의 기준
+select deptno, sum(sal), round(avg(sal)), count(*), count(comm), max(sal), min(sal)
+from emp
+group by deptno
+;
+
+-- 직무별 지표를 출력해보자
+select job,count(*) as "직원의 수", TO_CHAR(sum(sal), '999,999') as "급여의 총합", round(avg(sal)) as "급여의 평균", max(sal) as" 최대급여", min(sal) as "최소급여"
+from emp
+group by job
+;
--- sql/sql 과제2.sql
+++ sql/sql 과제2.sql
@@ -1,21 +1,22 @@
+
 --1 마당서점의고객이요구하는다음질문에대해SQL 문을작성하시오.
---(1) 도서번호가1인도서의이름
-select bookid,bookname
+--(1) 도서번호가1인 도서의이름
+select bookname
 from book
 where bookid = 1
 ;
---(2) 가격이20,000원이상인도서의이름
-select bookname,price
+--(2) 가격이 20,000원이상인 도서의이름
+select bookname
 from book
 where price >= 20000
 ;
 --(3) 박지성의총구매액(박지성의고객번호는1번으로놓고작성)
-select custid,saleprice
+select sum(saleprice) as "총구매액"
 from orders
 where custid = 1
 ;
 --(4) 박지성이구매한도서의수(박지성의고객번호는1번으로놓고작성
-select custid,bookid
+select count(*) as "구매한 도서의 수"
 from orders
 where custid=1
 ;
@@ -23,23 +24,24 @@ where custid=1
 
 --2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
 --(1) 마당서점도서의총개수
-select *
+select count(*) as "도서의 총개수"
 from book
 ;
 --(2) 마당서점에도서를출고하는출판사의총개수
-select publisher
-from book;
+select count(distinct(publisher)) as "출판사의 총개수"
+from book
+;
 --(3) 모든고객의이름, 주소
 select name, address
 from customer
 ;
 --(4) 2014년7월4일~7월7일사이에주문받은도서의주문번호
-select orderid, orderdate
+select orderid
 from orders
 where orderdate between '14/07/04' and '14/07/07'
 ;
 --(5) 2014년7월4일~7월7일사이에주문받은도서를제외한도서의주문번호
-select orderid, orderdate
+select orderid
 from orders
 where orderdate not between '14/07/04' and '14/07/07'
 ;
